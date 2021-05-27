ed8fe189750410225fc1038c958224b277778869
Tortoise097 <zhanglinjuan51@163.com>
2021-05-28 03:02:39 +0800

    a skeleton

---
+++ PkuWenWenbackend/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for PkuWenWenbackend project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PkuWenWenbackend.settings')
+
+application = get_wsgi_application()
